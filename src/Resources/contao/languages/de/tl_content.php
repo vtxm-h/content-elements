128967f9aab6eedb49610f1ddaca609520de6031
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-04-16 17:04:59 +0200

    Create tl_content.php

---
+++ src/Resources/contao/languages/de/tl_content.php
@@ -0,0 +1,56 @@
+<?php
+
+$GLOBALS['TL_LANG']['CTE']['vtxm_live_teaser'] = ['Live Teaser', 'Konzert-/Live-Ankündigungsblock.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_quote_teaser'] = ['Quote Teaser', 'Zitat-/Review-Block.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_announcement'] = ['Announcement', 'Ankündigungsblock.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_timeline'] = ['Timeline', 'Zeitstrahl-Block.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_tabs'] = ['Tabs', 'Tab-Element.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_accordion'] = ['Accordion', 'Accordion-Element.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_members_grid'] = ['Members Grid', 'Mitglieder-Grid.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_feature_grid'] = ['Feature Grid', 'Grid-Container für Feature-Items.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_feature_item'] = ['Feature Item', 'Einzelnes Feature-Element für ein Feature-Grid.'];
+
+$GLOBALS['TL_LANG']['tl_content']['liveDate'] = ['Datum', 'Datum oder Zeitraum.'];
+$GLOBALS['TL_LANG']['tl_content']['liveLocation'] = ['Ort', 'Venue oder Stadt.'];
+$GLOBALS['TL_LANG']['tl_content']['liveText'] = ['Text', 'Beschreibung.'];
+$GLOBALS['TL_LANG']['tl_content']['liveLink'] = ['Link', 'Optionaler Link.'];
+$GLOBALS['TL_LANG']['tl_content']['liveLinkText'] = ['Link-Text', 'Beschriftung des Links.'];
+
+$GLOBALS['TL_LANG']['tl_content']['quoteText'] = ['Zitat', 'Zitattext.'];
+$GLOBALS['TL_LANG']['tl_content']['quoteAuthor'] = ['Autor', 'Name der Person oder Quelle.'];
+$GLOBALS['TL_LANG']['tl_content']['quoteMeta'] = ['Meta', 'Zusatzinfo wie Medium oder Jahr.'];
+$GLOBALS['TL_LANG']['tl_content']['quoteLink'] = ['Link', 'Optionaler Link.'];
+$GLOBALS['TL_LANG']['tl_content']['quoteLinkText'] = ['Link-Text', 'Beschriftung des Links.'];
+
+$GLOBALS['TL_LANG']['tl_content']['announcementEyebrow'] = ['Eyebrow', 'Kleine Oberzeile.'];
+$GLOBALS['TL_LANG']['tl_content']['announcementText'] = ['Text', 'Ankündigungstext.'];
+$GLOBALS['TL_LANG']['tl_content']['announcementLink'] = ['Link', 'Optionaler Link.'];
+$GLOBALS['TL_LANG']['tl_content']['announcementLinkText'] = ['Link-Text', 'Beschriftung des Links.'];
+$GLOBALS['TL_LANG']['tl_content']['announcementStyle'] = ['Stil', 'Visueller Stil.'];
+
+$GLOBALS['TL_LANG']['tl_content']['timelineTitle'] = ['Timeline-Titel', 'Optionaler Titel oberhalb der Einträge.'];
+$GLOBALS['TL_LANG']['tl_content']['timelineItems'] = ['Timeline-Einträge', 'JSON-Array mit year/text-Einträgen.'];
+
+$GLOBALS['TL_LANG']['tl_content']['tabsStyle'] = ['Tabs-Stil', 'Visueller Stil.'];
+$GLOBALS['TL_LANG']['tl_content']['tabsItems'] = ['Tabs', 'JSON-Array mit title/content-Einträgen.'];
+
+$GLOBALS['TL_LANG']['tl_content']['accordionStyle'] = ['Accordion-Stil', 'Visueller Stil.'];
+$GLOBALS['TL_LANG']['tl_content']['accordionItems'] = ['Accordion-Einträge', 'JSON-Array mit title/content-Einträgen.'];
+
+$GLOBALS['TL_LANG']['tl_content']['memberImageTop'] = ['Bild oben', 'Mitgliedsbild oben.'];
+$GLOBALS['TL_LANG']['tl_content']['memberNameTop'] = ['Name oben', 'Mitgliedsname oben.'];
+$GLOBALS['TL_LANG']['tl_content']['memberImageLeft'] = ['Bild links', 'Mitgliedsbild links.'];
+$GLOBALS['TL_LANG']['tl_content']['memberNameLeft'] = ['Name links', 'Mitgliedsname links.'];
+$GLOBALS['TL_LANG']['tl_content']['memberImageRight'] = ['Bild rechts', 'Mitgliedsbild rechts.'];
+$GLOBALS['TL_LANG']['tl_content']['memberNameRight'] = ['Name rechts', 'Mitgliedsname rechts.'];
+$GLOBALS['TL_LANG']['tl_content']['memberImageBottom'] = ['Bild unten', 'Mitgliedsbild unten.'];
+$GLOBALS['TL_LANG']['tl_content']['memberNameBottom'] = ['Name unten', 'Mitgliedsname unten.'];
+
+$GLOBALS['TL_LANG']['tl_content']['fgStyle'] = ['Grid-Stil', 'Darstellungsart des Feature-Grids.'];
+$GLOBALS['TL_LANG']['tl_content']['fgColumns'] = ['Spalten', 'Anzahl Spalten.'];
+$GLOBALS['TL_LANG']['tl_content']['fgTheme'] = ['Theme', 'Hell oder dunkel.'];
+
+$GLOBALS['TL_LANG']['tl_content']['fiIcon'] = ['Icon', 'Icon-Code, SVG oder Markup.'];
+$GLOBALS['TL_LANG']['tl_content']['fiText'] = ['Text', 'Beschreibungstext.'];
+$GLOBALS['TL_LANG']['tl_content']['fiLink'] = ['Link', 'Optionaler Link.'];
+$GLOBALS['TL_LANG']['tl_content']['fiLinkText'] = ['Link-Text', 'Beschriftung des Links.'];
