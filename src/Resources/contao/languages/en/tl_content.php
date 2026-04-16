b1ff0f4185c2482f7911812ccbbb971a6b612abc
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-04-16 17:05:26 +0200

    Add language definitions for content elements
    Added language definitions for various content elements including live teaser, quote teaser, announcements, and more.

---
+++ src/Resources/contao/languages/en/tl_content.php
@@ -0,0 +1,56 @@
+<?php
+
+$GLOBALS['TL_LANG']['CTE']['vtxm_live_teaser'] = ['Live teaser', 'Concert / live announcement block.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_quote_teaser'] = ['Quote teaser', 'Quote / review block.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_announcement'] = ['Announcement', 'Announcement block.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_timeline'] = ['Timeline', 'Timeline block.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_tabs'] = ['Tabs', 'Tabs element.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_accordion'] = ['Accordion', 'Accordion element.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_members_grid'] = ['Members grid', 'Members grid element.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_feature_grid'] = ['Feature grid', 'Grid container for feature items.'];
+$GLOBALS['TL_LANG']['CTE']['vtxm_feature_item'] = ['Feature item', 'Single feature element for a feature grid.'];
+
+$GLOBALS['TL_LANG']['tl_content']['liveDate'] = ['Date', 'Date or time span.'];
+$GLOBALS['TL_LANG']['tl_content']['liveLocation'] = ['Location', 'Venue or city.'];
+$GLOBALS['TL_LANG']['tl_content']['liveText'] = ['Text', 'Description.'];
+$GLOBALS['TL_LANG']['tl_content']['liveLink'] = ['Link', 'Optional link.'];
+$GLOBALS['TL_LANG']['tl_content']['liveLinkText'] = ['Link text', 'Label for the link.'];
+
+$GLOBALS['TL_LANG']['tl_content']['quoteText'] = ['Quote', 'Quote text.'];
+$GLOBALS['TL_LANG']['tl_content']['quoteAuthor'] = ['Author', 'Name of the person or source.'];
+$GLOBALS['TL_LANG']['tl_content']['quoteMeta'] = ['Meta', 'Additional info such as medium or year.'];
+$GLOBALS['TL_LANG']['tl_content']['quoteLink'] = ['Link', 'Optional link.'];
+$GLOBALS['TL_LANG']['tl_content']['quoteLinkText'] = ['Link text', 'Label for the link.'];
+
+$GLOBALS['TL_LANG']['tl_content']['announcementEyebrow'] = ['Eyebrow', 'Small line above the headline.'];
+$GLOBALS['TL_LANG']['tl_content']['announcementText'] = ['Text', 'Announcement text.'];
+$GLOBALS['TL_LANG']['tl_content']['announcementLink'] = ['Link', 'Optional link.'];
+$GLOBALS['TL_LANG']['tl_content']['announcementLinkText'] = ['Link text', 'Label for the link.'];
+$GLOBALS['TL_LANG']['tl_content']['announcementStyle'] = ['Style', 'Visual style.'];
+
+$GLOBALS['TL_LANG']['tl_content']['timelineTitle'] = ['Timeline title', 'Optional title above the entries.'];
+$GLOBALS['TL_LANG']['tl_content']['timelineItems'] = ['Timeline items', 'JSON array with year/text entries.'];
+
+$GLOBALS['TL_LANG']['tl_content']['tabsStyle'] = ['Tabs style', 'Visual style.'];
+$GLOBALS['TL_LANG']['tl_content']['tabsItems'] = ['Tabs', 'JSON array with title/content entries.'];
+
+$GLOBALS['TL_LANG']['tl_content']['accordionStyle'] = ['Accordion style', 'Visual style.'];
+$GLOBALS['TL_LANG']['tl_content']['accordionItems'] = ['Accordion items', 'JSON array with title/content entries.'];
+
+$GLOBALS['TL_LANG']['tl_content']['memberImageTop'] = ['Top image', 'Top member image.'];
+$GLOBALS['TL_LANG']['tl_content']['memberNameTop'] = ['Top name', 'Top member name.'];
+$GLOBALS['TL_LANG']['tl_content']['memberImageLeft'] = ['Left image', 'Left member image.'];
+$GLOBALS['TL_LANG']['tl_content']['memberNameLeft'] = ['Left name', 'Left member name.'];
+$GLOBALS['TL_LANG']['tl_content']['memberImageRight'] = ['Right image', 'Right member image.'];
+$GLOBALS['TL_LANG']['tl_content']['memberNameRight'] = ['Right name', 'Right member name.'];
+$GLOBALS['TL_LANG']['tl_content']['memberImageBottom'] = ['Bottom image', 'Bottom member image.'];
+$GLOBALS['TL_LANG']['tl_content']['memberNameBottom'] = ['Bottom name', 'Bottom member name.'];
+
+$GLOBALS['TL_LANG']['tl_content']['fgStyle'] = ['Grid style', 'Feature grid presentation style.'];
+$GLOBALS['TL_LANG']['tl_content']['fgColumns'] = ['Columns', 'Number of columns.'];
+$GLOBALS['TL_LANG']['tl_content']['fgTheme'] = ['Theme', 'Light or dark theme.'];
+
+$GLOBALS['TL_LANG']['tl_content']['fiIcon'] = ['Icon', 'Icon code, SVG or markup.'];
+$GLOBALS['TL_LANG']['tl_content']['fiText'] = ['Text', 'Description text.'];
+$GLOBALS['TL_LANG']['tl_content']['fiLink'] = ['Link', 'Optional link.'];
+$GLOBALS['TL_LANG']['tl_content']['fiLinkText'] = ['Link text', 'Label for the link.'];
