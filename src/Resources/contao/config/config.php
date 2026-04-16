4dc3067fe9ee03a40259f2021569abb51ce55da3
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-04-16 16:49:36 +0200

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+$GLOBALS['TL_CTE']['vtxm']['vtxm_accordion']
+    = \Vendor\ContentElementsBundle\ContentElement\ContentAccordion::class;
+
+$GLOBALS['TL_CTE']['vtxm']['vtxm_tabs']
+    = \Vendor\ContentElementsBundle\ContentElement\ContentTabs::class;
+
+$GLOBALS['TL_CTE']['vtxm']['vtxm_members_grid']
+    = \Vendor\ContentElementsBundle\ContentElement\ContentMembersGrid::class;
+
+$GLOBALS['TL_CTE']['vtxm']['vtxm_live_teaser']
+    = \Vendor\ContentElementsBundle\ContentElement\ContentLiveTeaser::class;
+
+$GLOBALS['TL_CTE']['vtxm']['vtxm_timeline']
+    = \Vendor\ContentElementsBundle\ContentElement\ContentTimeline::class;
+
+$GLOBALS['TL_CTE']['vtxm']['vtxm_quote_teaser']
+    = \Vendor\ContentElementsBundle\ContentElement\ContentQuoteTeaser::class;
+
+$GLOBALS['TL_CTE']['vtxm']['vtxm_announcement']
+    = \Vendor\ContentElementsBundle\ContentElement\ContentAnnouncement::class;
+
+$GLOBALS['TL_CTE']['vtxm']['vtxm_feature_grid']
+    = \Vendor\ContentElementsBundle\ContentElement\ContentFeatureGrid::class;
+
+$GLOBALS['TL_CTE']['vtxm']['vtxm_feature_item']
+    = \Vendor\ContentElementsBundle\ContentElement\ContentFeatureItem::class;
